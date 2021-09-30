9a11207d86b0fbfa7d8c82e12ee803a9c2d40ac3
Asmaa <Asmaa99musse@gmail.com>
2021-09-30 13:58:00 +0100

    changed file placement, added pseudo code

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,17 +1,32 @@
 // Assignment Code
-var generateBtn = document.querySelector('#generate');
+var generateBtn = document.querySelector("#generate");
 
+//Declare a function 'generatePassword'
 function generatePassword() {
-  // PLEASE ADD ALL YOUR CODE HERE
+  //Declare the constant variables
+  const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numbers = "0123456789";
+  const specials = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+  //create const for password length
+  const passwordLength = 10;
+
+  //Declare variables for each DOM element
+  const length = document.getElementById("length");
+  const isLowerCase = document.getElementById("lowerCase");
+  const isUpperCase = document.getElementById("upperCase");
+  const isNumbers = document.getElementById("numbers");
+  const isSpecials = document.getElementById("specials");
 }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener("click", writePassword);
