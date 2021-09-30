7075c5fcf488fb8b80ea660b7549822f6f4d32c7
Asmaa <Asmaa99musse@gmail.com>
2021-09-30 23:40:18 +0100

    added length variable

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -21,37 +21,13 @@ function generatePassword() {
   const isSpecials = determineSpecials();
 }
 
-// var length = Number(prompt("How many characters will your password be? Enter a number between 8 and 128"));
-
-//ask for character type
-// var charType = prompt(
-//   "Enter a character type: special, numeric, uppercase, lowercase."
-// );
-
-//generate password
-// function generatePassword() {
-//   //evaluate character type
-//   var charSet = "";
-//   var charTypeLower = charType.toLowerCase();
-//   if( charTypeLower === "lowercase" ) {
-//     charSet = "abcdefghijklmnopqrstuvwxyz";
-//   } else if( charTypeLower === "uppercase" ) {
-//     charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//   } else if( charTypeLower === "numeric" ) {
-//     charSet = "0123456789";
-//   } else if( charTypeLower === "special" ) {
-//     charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-//   }
-//   //return value
-//   var retVal = "";
-//   for (var i = 0; i < length; i++) {
-//     //picks a character within charSet at index of random number
-//     retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
-//   }
-//   // return retVal;
-// }
-// alert(generatePassword());
-
+function determineLength() {
+  length = prompt
+  
+  if (length = 10) {
+    length = 10
+  } else  ...  
+}
 
 //Functions to determine what user wants to include
 function determineLowerCase() {
@@ -108,6 +84,17 @@ function determineSpecials() {
 
 // Run Validation
 
+//   //return value
+//   var retVal = "";
+//   for (var i = 0; i < length; i++) {
+//     //picks a character within charSet at index of random number
+//     retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+//   }
+//   // return retVal;
+// }
+// alert(generatePassword());
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -117,4 +104,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); 
