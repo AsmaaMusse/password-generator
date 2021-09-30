2e483f84ff045478e6aed9e5a166bb239a15568e
Asmaa <Asmaa99musse@gmail.com>
2021-09-30 21:40:43 +0100

    added if...else statements to js

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -20,18 +20,90 @@ function generatePassword() {
   const isNumbers = determineNumbers();
   const isSpecials = determineSpecials();
 }
+
+// var length = Number(prompt("How many characters will your password be? Enter a number between 8 and 128"));
+
+//ask for character type
+// var charType = prompt(
+//   "Enter a character type: special, numeric, uppercase, lowercase."
+// );
+
+//generate password
+// function generatePassword() {
+//   //evaluate character type
+//   var charSet = "";
+//   var charTypeLower = charType.toLowerCase();
+//   if( charTypeLower === "lowercase" ) {
+//     charSet = "abcdefghijklmnopqrstuvwxyz";
+//   } else if( charTypeLower === "uppercase" ) {
+//     charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//   } else if( charTypeLower === "numeric" ) {
+//     charSet = "0123456789";
+//   } else if( charTypeLower === "special" ) {
+//     charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+//   }
+//   //return value
+//   var retVal = "";
+//   for (var i = 0; i < length; i++) {
+//     //picks a character within charSet at index of random number
+//     retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+//   }
+//   // return retVal;
+// }
+// alert(generatePassword());
+
+
+//Functions to determine what user wants to include
 function determineLowerCase() {
-  lowerCaseCheck = confirm;
-  ("Do you want to include Lowercase Characters? (y/n)");
+  lowerCaseCheck = confirm (
+    "Do you want to include a Lowercase Characters? (y/n)"
+  );
+
+  if (lowerCaseCheck === "y") {
+    lowerCaseCheck = true;
+  } else if (lowerCaseCheck === "n") {
+    lowerCaseCheck = false;
+  } else (lowerCaseCheck === null); {
+    return ('Please choose y/n as a valid answer')
 }
 
 function determineUpperCase() {
-  upperCaseCheck = confirm;
-  ("Do you want to include Uppercase Characters? (y/n)");
+  upperCaseCheck = confirm (
+    "Do you want to include a Uppercase Characters? (y/n)"
+  );
+
+  if (upperCaseCheck === "y") {
+    upperCaseCheck = true;
+  } else if (upperCaseCheck === "n") {
+    upperCaseCheck = false;
+  } else (upperCaseCheck === null); {
+    return ('Please choose y/n as a valid answer')
 }
 
 function determineNumbers() {
-  numberCheck;
+  numberCheck = confirm (
+    "Do you want to include a Number? (y/n)"
+  );
+
+  if (numberCheck === "y") {
+    numberCheck = true;
+  } else if (upperCaseCheck === "n") {
+    numberCheck = false;
+  } else (numberCheck === null); {
+    return ('Please choose y/n as a valid answer')
+}
+
+function determineSpecials() {
+  specialCheck = confirm (
+    "Do you want to include a Special character? (y/n)"
+  );
+
+  if (specialCheck === "y") {
+    specialCheck = true;
+  } else if (specialCheck === "n") {
+    specialCheck = false;
+  } else (specialCheck === null); {
+    return ('Please choose y/n as a valid answer')
 }
 
 // Run Validation
