1fad88818517acad1174d1e6413ca980ca9e6327
Asmaa <Asmaa99musse@gmail.com>
2021-10-02 00:14:38 +0100

    added random password function

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -2,60 +2,60 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+  //Declare the constant variable
+  const lowercase = "abcdefghijklmnopqrstuvwxyxz";
+  const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numbers = "0123456789";
+  const specials = "!@#$%^&*()?/;:~";
 
- //Declare the constant variable
- const lowercase = 'abcdefghijklmnopqrstuvwxyxz';
- const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
- const numbers = '0123456789';
- const specials = '!@#$%^&*()?/;:~';
- const characters ='abcdefghijklmnopqrstuvwxyxzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()?/;:~';
+  //Determine password length
+  const length = 10;
+  const characters = lowercase + uppercase + numbers + specials;
+  const charactersLength = characters.length;
 
- //Determine password length
- const characters = 10;
- const characters = lowercase + uppercase + numbers + specials;
- const charactersLength = characters.length;
+  //Functions to determine what user wants to include
+  const isLowerCase = confirm("Do you want to add a lowercase?");
+  const isUpperCase = confirm("Do you want to add a uppercase?");
+  const isNumbers = confirm("Do you want to add a number?");
+  const isSpecials = confirm("Do you want to add a special character?");
 
-
- //Declare variables for each DOM element
- const isLength = determineLength();
- const isLowerCase = determineLowerCase();
- const isUpperCase = determineUpperCase();
- const isNumbers = determineNumbers();
- const isSpecials = determineSpecials();
-
-
-
- //Functions to determine what user wants to include
- const isLowerCase = confirm ('Do you want to add a lowercase?');
- const isUpperCase = confirm ('Do you want to add a uppercase?');
- const isNumbers = confirm ('Do you want to add a number?');
- const isSpecials = confirm ('Do you want to add a special character?');
-
-
- const isLowerCase = true;
- const isUpperCase = true;
- const isNumbers = true;
- const isSpecials = true;
+  isLowerCase = true / false;
+  isUpperCase = true / false;
+  isNumbers = true / false;
+  isSpecials = true / false;
 
   // Run Validation
-  if (!isLowerCase && isUpperCase && isNumbers && !isSpecials) {
-    alert ('Please choose at least 2 of the 4 options');
-    return; 
+  if (!isLowerCase && !isUpperCase && !isNumbers && !isSpecials) {
+    alert("Please choose at least 2 of the 4 options");
+    return;
   }
 
-  // Return value 
-  const choices = ['']
-        choices = [[lowercase], [uppercase], [numbers], [specials]];
+  // Return value
+  const choices = [""];
+  choices = [[lowercase], [uppercase], [numbers], [specials]];
 
- //Declare a function 'generatePassword'
-  function generatePassword() {
-   for (var i = 0; i < length; i++) {
-    password += characters.charAt(Math.floor(Math.random() * charactersLength));
-
-    return password;
+  if (isLowerCase) {
+    return choices[0];
   }
-}   
 
+  if (isUpperCase) {
+    return choices[1];
+  }
+
+  if (isNumbers) {
+    return choices[2];
+  }
+
+  if (isSpecials) {
+    return choices[3];
+  }
+
+  //Declare random password
+  for (let i = 0; i < length; i++) {
+    randomNumber = Math.floor(Math.random() * characters.length);
+    password += characters.substring(randomNumber, randomNumber + 1);
+  }
+}
 
 // Write password to the #password input
 function writePassword() {
